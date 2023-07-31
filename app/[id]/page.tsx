fe108c0124987bf8b69d3bccb6b45aebbd36ff52
alihaghjou <alihaghjou30@gmail.com>
2023-07-31 11:15:16 +0330

    main page data added and single page created

---
+++ app/[id]/page.tsx
@@ -0,0 +1,12 @@
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs"
+import {cookies} from "next/headers"
+
+export default async function page({params}: {params: {id: string}}) {
+
+    const supabase = createServerComponentClient({cookies})
+    const {data: product} = await supabase.from("product").select().eq("id", params.id).maybeSingle()
+
+  return (
+    <div>{product.title}</div>
+  )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,42 @@
-import { createServerComponentClient } from "@supabase/auth-helpers-nextjs"
-import {cookies} from "next/headers"
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+import Image from "next/image";
+
+interface product {
+  id: number,
+  title: string,
+  description: string,
+  price: number,
+  image: string
+}
 
 export default async function Home() {
-  const supabase = createServerComponentClient({cookies})
+  const supabase = createServerComponentClient({ cookies });
 
-  const ob = await supabase.from("product").select()
-
+  const { data: products, error } = await supabase.from("product").select().limit(10);
+  console.log(error)
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <button className="btn btn-outline btn-info">Info</button>
+    <main className="flex min-h-screen items-center justify-center p-24">
+      {products?.map((product: product) => (
+        <div key={product.id} className="card w-96 bg-base-100 shadow-xl">
+          <figure>
+            <Image
+              src={product.image}
+              alt="Shoes"
+              width={256}
+              height={256}
+              className="w-full"
+            />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">{product.title}</h2>
+            <p>{product.description}</p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-outline btn-info">Buy Now</button>
+            </div>
+          </div>
+        </div>
+      ))}
     </main>
-  )
+  );
 }
