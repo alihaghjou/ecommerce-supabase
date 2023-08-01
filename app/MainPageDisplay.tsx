9c453beaaa913560a6e5e3b981d219c6b47f5da6
alihaghjou <alihaghjou30@gmail.com>
2023-08-01 23:07:57 +0330

    user authentication added

--- app/MainPageDisplay.tsx
+++ app/MainPageDisplay.tsx
@@ -1,11 +1,28 @@
-"use client"
+"use client";
 import Image from "next/image";
 import Link from "next/link";
-import { product } from './page'
+import { product } from "./page";
+import LogoutButton from "./LogoutButton";
+import { User } from "@supabase/supabase-js";
 
-export default function MainPageDisplay({products}: {products: product[]}) {
+export default function MainPageDisplay({ products, user }: { products: product[], user: User | null }) {
   return (
-    <main className="flex min-h-screen items-center justify-center p-24">
+    <main className="flex min-h-screen items-center justify-center p-24 animate-in">
+      <div>
+        {user ? (
+          <div className="flex items-center gap-4">
+            Hey, {user.email}!
+            <LogoutButton />
+          </div>
+        ) : (
+          <Link
+            href="/login"
+            className="py-2 px-4 rounded-md no-underline bg-btn-background hover:bg-btn-background-hover"
+          >
+            Login
+          </Link>
+        )}
+      </div>
       {products?.map((product: product) => (
         <div key={product.id} className="card w-96 bg-base-100 shadow-xl">
           <figure>
@@ -21,11 +38,13 @@ export default function MainPageDisplay({products}: {products: product[]}) {
             <h2 className="card-title">{product.title}</h2>
             <p>{product.description}</p>
             <div className="card-actions justify-end">
-              <Link href={`/${product.id}`}><button className="btn btn-outline btn-info">Buy Now</button></Link>
+              <Link href={`/${product.id}`}>
+                <button className="btn btn-outline btn-info">Buy Now</button>
+              </Link>
             </div>
           </div>
         </div>
       ))}
     </main>
-  )
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -2,21 +2,23 @@ import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
 import MainPageDisplay from "./MainPageDisplay";
 
-
 export interface product {
-  id: number,
-  title: string,
-  description: string,
-  price: number,
-  image: string
+  id: number;
+  title: string;
+  description: string;
+  price: number;
+  image: string;
 }
 
 export default async function Home() {
   const supabase = createServerComponentClient({ cookies });
-  console.log(cookies)
-  const { data: products, error } = await supabase.from("product").select().limit(10);
-  if (error) throw new Error(error.message)
-  return (
-    <MainPageDisplay products={products} />
-  );
+
+  const {data: {user}} = await supabase.auth.getUser()
+  
+  const { data: products, error } = await supabase
+    .from("product")
+    .select()
+    .limit(10);
+  if (error) throw new Error(error.message);
+  return <MainPageDisplay products={products} user={user} />;
 }
